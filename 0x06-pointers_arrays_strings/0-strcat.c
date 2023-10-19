fec2c702c34cc857f13555527ffc7a7f978ccf01
Thapelo <telekone@gmail.com>
2023-10-19 04:13:24 -0700

    Concatenating strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,7 +4,7 @@
  * _strcat - concatenates two strings
  * @dest: input value
  * @src: input value
- * Retruen: void
+ * Return: void
  */
 char *_strcat(char *dest, char *src)
 {
@@ -12,7 +12,8 @@ char *_strcat(char *dest, char *src)
 	int j;
 
 	i = 0;
-	while (dest[i] != '\0');
+
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
