a54308098a9afc684db6868680d451157d6e608c
Thapelo <telekone@gmail.com>
2023-10-20 03:55:00 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,9 +13,6 @@ int main(void)
 	printf("size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("size of a float: %zu byte(s)\n", sizeof(float));
-	(144 chars long)
-	[stderr] : [Anything]
-	(0 chars long)
 
 	return (0);
 }
