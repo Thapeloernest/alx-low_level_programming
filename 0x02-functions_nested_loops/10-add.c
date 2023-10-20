8c2f677ea155f390d34dbe3b649c19fb7845218a
Thapelo <telekone@gmail.com>
2023-10-20 16:00:38 -0700

    adding to numbers and returning the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds the integers and returns the results
+ *
+ * @x: number being added
+ *
+ * @y: number being added
+ *
+ * Return: Always 0.
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
