ed7c645e1a20b6f4011b5924d061c69bf3463d2a
Thapelo <telekone@gmail.com>
2023-10-20 12:29:44 -0700

    Understanding functions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,16 @@
-#include <stdio.h>
 #include "main.h"
-
 /**
  * print_alphabet - the function of this task
  * Return: return 0
  */
 void print_alphabet(void)
 {
-	char c;
+char c;
 
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		 _putchar(c);
-	}
-		_putchar('\n');
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
 }
 
