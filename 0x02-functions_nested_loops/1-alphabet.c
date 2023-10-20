270f131cc82869fc40260038881b5283242e6544
Thapelo <telekone@gmail.com>
2023-10-20 12:09:27 -0700

    Printing alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - the function of this task
+ * Return: return 0
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		 _putchar(c);
+	}
+		_putchar('\n');
+}
+
