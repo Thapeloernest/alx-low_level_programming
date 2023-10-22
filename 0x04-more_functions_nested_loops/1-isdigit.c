4ee77d822611b89a45549d99286f132ab4421615
Thapelo <telekone@gmail.com>
2023-10-22 03:02:30 -0700

    checking for a digit from 0 -9

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,4 +1,4 @@
-#iclude "main.h"
+#include "main.h"
 
 /**
  * _isdigit - check if numbers are 0 - 9
@@ -9,8 +9,5 @@
 
 int _isdigit(int c)
 {
-	if (c >= '0' && c <= '9')
-		return (1);
-	else
-		return (0);
+return (c >= '0' && c <= '9');
 }
--- 0x04-more_functions_nested_loops/1-main.c
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -8,11 +8,11 @@ nclude "main.h"
  *     */
 int main(void)
 {
-	    char c;
+	char c;
 
-	        c = '0';
-		    printf("%c: %d\n", c, _isdigit(c));
-		        c = 'a';
-			    printf("%c: %d\n", c, _isdigit(c));
-			        return (0);
+	c = '0';
+	printf("%c: %d\n", c, _isdigit(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isdigit(c));
+	return (0);
 }
