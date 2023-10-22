4f47cd21f50dc9151aa25fa12ac09850d839edd0
Thapelo <telekone@gmail.com>
2023-10-22 03:41:51 -0700

    Printing numbers from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * print_numbers - print 0-9
- * return: void
+ * print_numbers - function prints for numbers from 0 to 9
+ * return: Always 0.
  */
 
 void print_numbers(void)
 {
-	char c,
-	
-	for (c='0', c<='9', c++)
+	char c;
+
+	for (c = 48; c <= 58; c++)
 	{
 		_putchar(c);
 	}
-	_putchar("\n");
+	_putchar('\n');
 }
