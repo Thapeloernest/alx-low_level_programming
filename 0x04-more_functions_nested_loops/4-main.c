47ccc79b5c342e764ab79fab075efc2912071219
Thapelo <telekone@gmail.com>
2023-10-22 04:04:52 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-main.c
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- *  * main - check the code
- *   *
- *    * Return: Always 0.
- *     */
+ * print_most_numbers - this function checks
+ * digits from 0 to 9
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	    print_most_numbers();
-	        return (0);
+	print_most_numbers();
+	return (0);
 }
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,11 +8,18 @@
 
 void print_most_numbers(void)
 {
-	char c;
-	for (c='0', c<='9', c++)
+	int c;
+
+	for (c = 48; c < 58; c++)
 	{
-		if(!(c == '2' || c == '4'))
-			_putchar(c);
+
+		if (c != 50)
+		{
+			if (c != 52)
+			{
+				_putchar(c);
+			}
+		}
 	}
 	_putchar('\n');
 }
