d1ba10116a75a09d7cc1bda3561e465a3030a365
Thapelo <telekone@gmail.com>
2023-10-22 03:16:21 -0700

    Multiplying 2 numbers and returning result

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,7 +4,7 @@
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
-void mul(int a, int b);
+int mul(int a, int b);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
